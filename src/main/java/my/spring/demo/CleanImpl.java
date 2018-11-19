17fc5a84f175dd0cb974caac0c900b39476af9b9
Evgeniy Bulanov <jeak110@gmail.com>
2018-11-19 10:00:05 +0700

    before task 2

--- src/main/java/my/spring/demo/CleanImpl.java
+++ src/main/java/my/spring/demo/CleanImpl.java
@@ -1,8 +1,13 @@
 package my.spring.demo;
 
 public class CleanImpl implements Cleaner {
+    @InjectRandomInt(min = 2, max = 5)
+    private Integer repeat = 3;
+
     public void clean() {
         // 100500 lines of logic
-        System.out.println("VVVVVVvvvvvvvvv");
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("VVVVVVvvvvvvvvv");
+        }
     }
 }
