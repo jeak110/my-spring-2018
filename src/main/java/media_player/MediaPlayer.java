9fa35a3c5be3ac58e75d8ea9d827bdc5ab82f1d2
Evgeniy Bulanov <jeak110@gmail.com>
2018-11-23 09:40:54 +0700

    circular dependency - DOESNT WORK

--- src/main/java/media_player/MediaPlayer.java
+++ src/main/java/media_player/MediaPlayer.java
@@ -12,7 +12,13 @@ public class MediaPlayer {
     @Getter
     private List<MediaDevice> mediaDevices;
 
+    public MediaPlayer(List<MediaDevice> mediaDevices) {
+        System.out.println("CONSTRUCTOR: MEDIA PLAYER");
+        this.mediaDevices = mediaDevices;
+    }
+
     public void init() {
+        System.out.println("INIT: MEDIA PLAYER");
         System.out.println("Initializing media player, media devices found");
         mediaDevices.forEach(e-> System.out.println(e.getClass().getName()));
 
