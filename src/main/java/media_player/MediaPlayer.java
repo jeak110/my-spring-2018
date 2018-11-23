51091f8b6e1d2f6e1aa3180c2f74bedca6b82e61
Evgeniy Bulanov <jeak110@gmail.com>
2018-11-23 09:49:30 +0700

    @PostConstruct

--- src/main/java/media_player/MediaPlayer.java
+++ src/main/java/media_player/MediaPlayer.java
@@ -4,6 +4,7 @@ package media_player;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 public class MediaPlayer {
@@ -12,6 +13,7 @@ public class MediaPlayer {
     @Getter
     private List<MediaDevice> mediaDevices;
 
+    @PostConstruct
     public void init() {
         System.out.println("Initializing media player, media devices found");
         mediaDevices.forEach(e-> System.out.println(e.getClass().getName()));
