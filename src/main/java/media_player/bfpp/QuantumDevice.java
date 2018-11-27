f2dd33f80ccaf166791eba054caffb4af6abe731
Evgeniy Bulanov <jeak110@gmail.com>
2018-11-27 11:13:12 +0700

    profile 'research'

--- src/main/java/media_player/bfpp/QuantumDevice.java
+++ src/main/java/media_player/bfpp/QuantumDevice.java
@@ -3,7 +3,12 @@ package media_player.bfpp;
 import lombok.Setter;
 import media_player.Disk;
 import media_player.MediaDevice;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@Profile("research")
 public class QuantumDevice implements MediaDevice {
     @Override
     public void play() {
@@ -11,7 +16,7 @@ public class QuantumDevice implements MediaDevice {
     }
 
     public void init() {
-
+        System.out.println("INIT QUANTUM DEVICE");
     }
 
     @Setter
